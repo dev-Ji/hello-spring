6d53ca137d6dafc8851be35eecfc4b571631bb16
dev-Ji <wendyyi@naver.com>
2021-05-23 00:24:58 +0900

    2021.05.23 26화까지

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,11 +6,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 
-
+@Transactional
 public class MemberService {
 
     private MemberRepository memberRepository;
@@ -24,9 +25,10 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,14 +37,14 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
+
     /**
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
+
+            return memberRepository.findAll();
+
     }
 
 }
